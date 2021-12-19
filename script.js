43dc38dabac6e6f360e960c9bdcefc3424935c99
0amfibinator0 <00amfibiantor00@mail.ru>
2021-12-19 22:20:18 +0300

    усложненка 7 урока

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-let number = 266219;
-let string = String(number); // превратили число в строку
-let array = string.split(''); // превратили строку в массив
-let answer = array[0] * array[1] * array[2] * array[3] * array[4] * array[5];
+let week = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+let date = new Date();
 
-console.log(answer);
-
-let num = answer ** 3; // возводим степень
-console.log(num);
-let rezult = String(num);
-let arr = rezult.split('');
-
-console.log(arr[0], arr[1]);
+for (let i = 0; i < week.length; i++) {
+    const div = document.createElement("div");
+    if (i === 0 || i === 6) {
+        div.style.fontStyle = "italic";
+    }
+    if ( i === date.getDay() ) {
+        div.style.fontWeight = "bolder";
+    }
+    div.textContent = week[i];
+    document.body.append(div);
+}
