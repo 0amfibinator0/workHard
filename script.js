28a9e0afd6765473ce37dd34218b9ea49d86dd28
0amfibinator0 <00amfibiantor00@mail.ru>
2021-12-25 21:47:34 +0300

    исправил

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 const div = document.createElement("div");
 const dove = document.createElement("div");
 
+let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+let monthes = ['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня',
+'Июля', 'Августа', 'Сентября', 'Октября', 'Ноября', 'Декабря'];
+
 setInterval(function(){
-    let week = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
-    let monthes = ['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня', 'Июля', 'Августа', 'Сентября', 'Октября', 'Ноября', 'Декабря'];
     let date = new Date();
-    let day = date.getUTCDay();
+    let day = date.getUTCDay() - 1;
     let month = date.getMonth();
     let hour;
+    let minutes;
+    let seconds;
+
+    if (day < 0) {
+        day = 6;
+    }
 
     if (date.getHours() >= 2 && date.getHours() <= 4) {
         hour = "часа";
@@ -19,7 +27,25 @@ setInterval(function(){
         hour = "час";
     }
 
-    div.textContent = `Сегодня ${week[day]}, ${date.getDate()} ${monthes[month]} ${date.getFullYear()} года, ${date.getHours()} ${hour} ${date.getMinutes()} минут ${date.getSeconds()} секунды`;
+    if (date.getSeconds() % 10 >= 2 && date.getSeconds() % 10 <= 4) {
+        seconds = "секунды";
+    } else if (date.getSeconds() % 10 >= 5 || date.getSeconds() % 10 === 0) {
+        seconds = "секунд";
+    } else {
+        seconds = "секунда";
+    }
+
+    if (date.getMinutes() % 10 >= 2 && date.getMinutes() % 10 <= 4) {
+        minutes = "минуты";
+    } else if (date.getMinutes() % 10 >= 5 || date.getMinutes() <= 20 || date.getMinutes() % 10 === 0) {
+        minutes = "минут";
+    } else {
+        minutes = "минута";
+    }
+
+    div.textContent = `Сегодня ${week[day]},
+    ${date.getDate()} ${monthes[month]} ${date.getFullYear()} года,
+    ${date.getHours()} ${hour} ${date.getMinutes()} ${minutes} ${date.getSeconds()} ${seconds}`;
 },1000);
 setInterval(function() {
     const date = new Date();
