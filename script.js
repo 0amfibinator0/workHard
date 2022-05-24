2eeab2bc861988347e41b4a9c17f5b3d8363361e
0amfibinator0 <00amfibiantor00@mail.ru>
2022-05-24 14:23:27 +0300

    усложненка 29 урока

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
 const firstSelect = document.getElementById('first-select');
 const firstInput = document.getElementById('first-input');
 const secondSelect = document.getElementById('second-select');
-const secondInput = document.getElementById('second-input');
 const myHeaders = new Headers();
 myHeaders.append("apikey", "GYJCAGYC5yRCTxujemFX8Qg7pedYZlUo");
 
+let secondInput = document.getElementById('second-input');
 let from = 'USD';
 let to = 'RUB';
 let amount = 1;
 
 async function getData () {
-    const response = await fetch(`https://api.apilayer.com/exchangerates_data/convert?to=${to}&from=${from}&amount=${amount}`, {
+    return await fetch(`https://api.apilayer.com/exchangerates_data/convert?to=${to}&from=${from}&amount=${amount}`, {
         method: 'GET',
         redirect: 'follow',
         headers: myHeaders
     })
     .then(response => response.json())
-    .then(result => console.log(result))
+    .then(result => secondInput.value = result.result)
     .catch(error => console.log('error', error));
-    console.log(response);
 }
+firstSelect.addEventListener('change', ({target}) => {
+    from = target.value;
+    to = secondSelect.value;
+    getData();
+});
+secondSelect.addEventListener('change', ({target}) => {
+    from = firstSelect.value;
+    to = target.value;
+    getData();
+});
 getData();
 
 
